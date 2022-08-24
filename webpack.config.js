49c9a200c43aa32ab737dc241cae0f90ab51bb05
alfred <alfredgg@yahoo.es>
2022-08-24 17:56:10 +0200

    Fix settings page

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,12 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: [
+              '@babel/preset-env'
+            ],
+            plugins:[
+              ['@babel/transform-react-jsx', {pragma:'h'}]
+            ],
             cacheDirectory: true
           }
         }
